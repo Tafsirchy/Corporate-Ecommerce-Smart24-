79694e2b38ed30f43b1980a7e8509a219cc6e012
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-07 20:42:35 +0600

    chore: update categories.module.ts

--- apps/backend/src/categories/categories.module.ts
+++ apps/backend/src/categories/categories.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 
+import { AuditLogModule } from '../audit-log/audit-log.module';
+
 @Module({
+  imports: [AuditLogModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
