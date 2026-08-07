a4e3de11813ba901810fef129879d224edf74cf1
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-07 20:41:32 +0600

    chore: remove update-bulk-order.dto.ts

--- apps/backend/src/bulk-order/dto/update-bulk-order.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBulkOrderDto } from './create-bulk-order.dto';
-
-export class UpdateBulkOrderDto extends PartialType(CreateBulkOrderDto) {}
