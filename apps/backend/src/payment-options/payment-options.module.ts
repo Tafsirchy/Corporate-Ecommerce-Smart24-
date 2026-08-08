3ef70e7dab56cd416c6b4ffaa55367ae5cf3a0b8
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-08 22:57:18 +0600

    chore: update payment-options.module.ts

--- apps/backend/src/payment-options/payment-options.module.ts
+++ apps/backend/src/payment-options/payment-options.module.ts
@@ -5,6 +5,6 @@ import { PaymentOptionsController } from './payment-options.controller';
 @Module({
   controllers: [PaymentOptionsController],
   providers: [PaymentOptionsService],
-  exports: [PaymentOptionsService]
+  exports: [PaymentOptionsService],
 })
 export class PaymentOptionsModule {}
