9f6b1a171d20575bb69287105ea1f36a18e9cce4
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-08 22:57:17 +0600

    chore: update orders.module.ts

--- apps/backend/src/orders/orders.module.ts
+++ apps/backend/src/orders/orders.module.ts
@@ -3,11 +3,13 @@ import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { StripeModule } from '../stripe/stripe.module';
 import { LoyaltyModule } from '../loyalty/loyalty.module';
+import { OrderCreatedListener } from './listeners/order-created.listener';
+import { PricingService } from './pricing.service';
 
 @Module({
   imports: [forwardRef(() => StripeModule), LoyaltyModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, PricingService, OrderCreatedListener],
   exports: [OrdersService],
 })
 export class OrdersModule {}
