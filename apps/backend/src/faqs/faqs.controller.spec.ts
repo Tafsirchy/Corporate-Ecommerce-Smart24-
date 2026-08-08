65e86fa1889fce03a8253d7e697af57a8fe2a9ec
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-08 22:57:10 +0600

    chore: update faqs.controller.spec.ts

--- apps/backend/src/faqs/faqs.controller.spec.ts
+++ apps/backend/src/faqs/faqs.controller.spec.ts
@@ -7,7 +7,9 @@ describe('FaqsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FaqsController],
-    }).compile();
+    })
+      .useMocker(() => ({}))
+      .compile();
 
     controller = module.get<FaqsController>(FaqsController);
   });
