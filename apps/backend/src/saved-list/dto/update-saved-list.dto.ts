bdad3fccf104b0d44f3c1571a2519131b98b6bca
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-08 22:57:25 +0600

    chore: remove update-saved-list.dto.ts

--- apps/backend/src/saved-list/dto/update-saved-list.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSavedListDto } from './create-saved-list.dto';
-
-export class UpdateSavedListDto extends PartialType(CreateSavedListDto) {}
