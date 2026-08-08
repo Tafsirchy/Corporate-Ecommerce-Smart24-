782d217ce3ebd06da6fd0bf00b305c431c704d0f
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-08 22:57:23 +0600

    chore: update reviews.controller.ts

--- apps/backend/src/reviews/reviews.controller.ts
+++ apps/backend/src/reviews/reviews.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
