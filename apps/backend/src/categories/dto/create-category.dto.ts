11b769df0c6f972ef53d9d05d6dfc1ceaa0f863e
Tafsirchy <tafsirchy1000@gmail.com>
2026-08-10 22:53:06 +0600

    chore: refactor and cleanup apps/backend/src/categories/dto/create-category.dto.ts

--- apps/backend/src/categories/dto/create-category.dto.ts
+++ apps/backend/src/categories/dto/create-category.dto.ts
@@ -1,4 +1,10 @@
-import { IsString, IsOptional, IsMongoId, IsNotEmpty, IsBoolean } from 'class-validator';
+import {
+  IsString,
+  IsOptional,
+  IsMongoId,
+  IsNotEmpty,
+  IsBoolean,
+} from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
